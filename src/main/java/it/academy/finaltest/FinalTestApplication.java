7ccfa40605236e922c9453eb057218d9f4e3933a
ERERkin <erkin1399@gmail.com>
2020-06-22 20:59:27 +0600

    First Commit

---
+++ src/main/java/it/academy/finaltest/FinalTestApplication.java
@@ -0,0 +1,13 @@
+package it.academy.finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTestApplication.class, args);
+	}
+
+}
